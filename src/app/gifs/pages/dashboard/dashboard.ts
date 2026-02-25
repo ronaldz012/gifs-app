beeab7d6ec7c7ef813ffb3006218f01d7332458f
ronaldz012 <rzepita02@gmail.com>
2026-02-25 10:48:38 -0400

    refactor: components sidebar in firs folder

--- src/app/gifs/pages/dashboard/dashboard.ts
+++ src/app/gifs/pages/dashboard/dashboard.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import sidebar from '../../components/sidebar/sidebar/sidebar';
+import sidebar from '../../components/sidebar/sidebar';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
