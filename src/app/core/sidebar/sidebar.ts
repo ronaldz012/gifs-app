ee0482ae3513ee0a478ab2db35539712e5aedc26
ronaldz012 <rzepita02@gmail.com>
2026-05-01 18:05:22 -0400

    refactor: template inline

--- src/app/core/sidebar/sidebar.ts
+++ src/app/core/sidebar/sidebar.ts
@@ -9,7 +9,32 @@ import {SideBarService} from '../Dashboard/services/side-bar-service';
 @Component({
   selector: 'app-sidebar',
   imports: [SideMenuOption],
-  templateUrl: './sidebar.html',
+  template: `
+    @if (sidebarSvc.isOpen()) {
+      <div
+        class="fixed inset-0 bg-black/30 z-20 md:hidden"
+        (click)="sidebarSvc.close()">
+      </div>
+    }
+
+    <aside
+      class="fixed top-0 left-0 h-screen w-64 bg-blue-800 z-30 flex flex-col
+         md:sticky md:translate-x-0 md:z-auto"
+      [class.transition-transform]="ready()"
+      [class.duration-300]="ready()"
+      [class.ease-in-out]="ready()"
+      [class.-translate-x-full]="!sidebarSvc.isOpen()"
+      [class.translate-x-0]="sidebarSvc.isOpen()">
+
+      <div class="flex-1 overflow-y-auto">
+        <app-side-menu-option
+          [modules]="modules()"
+          (onNavigate)="sidebarSvc.close()" />
+      </div>
+
+    </aside>
+
+  `,
 })
 export default class Sidebar {
   readonly authService  = inject(AuthService);
